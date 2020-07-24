2dbaad88888f8cb9fd2a7d68a3ef6ae6cb59a672
Jason <Jason@mac>
2020-07-24 08:23:31 +0800

    add script to convert files to jpg

---
+++ convert2jpg.py
@@ -0,0 +1,28 @@
+#!/usr/local/bin/python
+from oiio import OpenImageIO as oiio
+import sys
+import os
+
+
+def convert(img):
+    filename, ext = os.path.splitext(img)
+    outfile = filename + ".jpg"
+    if ext == '.jpg':
+        return
+    if ext in ['.jpeg', '.JPG', '.JPEG']:        
+        os.rename(img, outfile)
+    else:
+        buf = oiio.ImageBuf(img)
+        buf.write(outfile)
+        os.remove(img)
+
+
+if __name__ == "__main__":
+    if (len(sys.argv) < 2):
+        print(f"sys.argv[0] image_folder/")
+        exit(1)
+
+    folder = sys.argv[1]
+    files = os.listdir(folder)
+    for f in files:
+        convert(os.path.join(folder, f))
